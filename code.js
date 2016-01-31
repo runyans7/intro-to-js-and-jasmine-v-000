9be4d56f543063bde69da8611e3578f11639a320
Scotty Runyan <runyans7@gmail.com>
2016-01-30 20:36:50 -0500

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(favorite_flavor) {
+  return 'I love ' + favorite_flavor;
+}
+
+function shouting(string) {
+  return string.toUpperCase();
+}
+
+function roundDown(integer) {
+  return Math.floor(integer);
+}
+
+function theTruth() {
+  return true;
+}
